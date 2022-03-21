2527587cb8d76de5ff4cb88320b7df348204149f
Vampire-Officail <bhanuprakashchitti44@gmail.com>
2022-03-21 15:14:27 +0530

    fixing user@user error

--- updaterepo.sh
+++ updaterepo.sh
@@ -1,2 +1,14 @@
 repo-add vampirerepo.db.tar.gz *.zst
 rm -rf *.old
+
+git add .
+
+git commit -m "fixing user@user error"
+
+git push
+
+sleep 10
+
+echo "######################################################"
+echo "                      done                             "
+echo "######################################################"
